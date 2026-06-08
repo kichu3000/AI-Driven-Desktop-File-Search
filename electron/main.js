d234723381ca83ce68baa46b4ea42fd72cc8523f
kichu <anandakrishnanvb3000@gmail.com>
2026-06-08 15:23:09 +0530

    Initial react and electrone setup

---
+++ electron/main.js
@@ -0,0 +1,16 @@
+import {app, BrowserWindow } from "electron";
+
+function createWindow() {
+    const win = new BrowserWindow({
+    width: 1200,
+    height: 1000,
+    });
+
+    win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+    app.quit();
+});
