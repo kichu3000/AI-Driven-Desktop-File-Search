8164c6196def21320be0eb4ce5ab0c73a9702ea8
kichu <anandakrishnanvb3000@gmail.com>
2026-06-10 16:31:11 +0530

    added the center text

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import TitleBar from "./components/Titlebar";
+import SearchPage from "./components/Searchpage";
 function App(){
     return(
         <div>
             <TitleBar/>
+            <SearchPage/>
         </div>
     )
 }
---
+++ src/components/Searchpage.jsx
@@ -0,0 +1,19 @@
+import "./SearchPage.css"
+
+function SearchPage(){
+    return(
+
+    <div className="container">
+        <div className="heading">
+            <div className="main-heading">
+                <h1>Find your files.</h1>
+            </div>
+            <div className="caption">
+                <p>Search using natural language</p>
+            </div>
+
+        </div>
+    </div>
+    )
+}
+export default SearchPage;
