522ad325aca5075136ce1cf66a4fff027e086198
kichu <anandakrishnanvb3000@gmail.com>
2026-06-11 16:26:44 +0530

    added button

--- src/components/Searchpage.jsx
+++ src/components/Searchpage.jsx
@@ -1,9 +1,20 @@
 import "./SearchPage.css"
 import { Search } from "lucide-react";
+import placeholder from "../assets/placeholder.json"
+import { useEffect,useState } from "react";
+import { GiButtonFinger } from "react-icons/gi";
 
 function SearchPage(){
 
-    let currentPlaceholder = "Life's too fast to remember every filename."
+    const[currentPlaceholder,setCurrentPlaceholder] = useState("");
+
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * placeholder.placeholders.length);
+        console.log(randomIndex);
+        setCurrentPlaceholder(placeholder.placeholders[randomIndex]);
+    },[]);
+    
+    
 
     return(
     <div className="container">
@@ -18,6 +29,9 @@ function SearchPage(){
             <div className="input-box">
                 <Search className="search-icon" size={20} />
                 <input type="text" name="input" id="input" placeholder={currentPlaceholder}/>
+            </div>
+            <div className="button">
+                <button type="submit">Search</button>
             </div>
 
         </div>
